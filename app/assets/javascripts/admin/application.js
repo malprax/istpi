e291a252f8a47b06b9fdb8a70378d610151e13e4
malprax <kingmalprax@gmail.com>
2016-07-06 22:30:13 +0800

    add progress again

---
+++ app/assets/javascripts/admin/application.js
@@ -0,0 +1,32 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require jquery_nested_form
+//= require chosen-jquery
+//= require jquery-ui
+
+//= require ckeditor/override
+//= require ckeditor/init
+//= require bootstrap-wysihtml5
+//= require bootstrap-datepicker
+//= require message_flash
+//= require lightbox-bootstrap
+//= require turbolinks
+//= require_tree .
+
+$(document).ready(function(){
+  $('body').scrollspy({ target: '#navbar-example' });
+
+});
