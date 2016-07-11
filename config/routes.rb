9d753f1951c69c095cea74fd8a12c2c450237856
malprax <kingmalprax@gmail.com>
2016-07-12 05:53:48 +0800

    modify admin/users/

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get "login" => 'sessions#new', :as => "login"
+    get "logout" => 'sessions#destroy', :as => "logout"
   end
 
   resources :lectures
