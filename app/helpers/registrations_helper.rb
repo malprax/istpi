8dae87156a274d601828d6a8fce97b22a7faea63
malprax <kingmalprax@gmail.com>
2016-10-06 17:47:05 +0800

    change icon action part 1

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,17 @@
 module RegistrationsHelper
+  def simbol_edit(value)
+   content_tag(:span, value, :class => "glyphicon glyphicon-edit")
+  end
+
+  def simbol_hapus(value)
+   content_tag(:span, value, :class => "glyphicon glyphicon-remove")
+  end
+
+  def simbol_cetak(value)
+   content_tag(:span, value, :class => "glyphicon glyphicon-print")
+  end
+
+  def simbol_ok(value)
+   content_tag(:span, value, :class => "glyphicon glyphicon-ok")
+  end
 end
