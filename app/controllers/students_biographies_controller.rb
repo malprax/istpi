df10fd2b48141ead8e008a552824970367c687b1
malprax <kingmalprax@gmail.com>
2016-10-09 21:41:46 +0800

    modified students biographies

---
+++ app/controllers/students_biographies_controller.rb
@@ -0,0 +1,24 @@
+class StudentsBiographiesController < ApplicationController
+  before_action :set_students_biography, only: [:show, :edit, :update, :destroy]
+
+  # GET /students_biographies
+  # GET /students_biographies.json
+  def index
+    if params[:search]
+        @students_biographies = StudentsBiography.paginate(:page => params[:page], per_page: 10).search(params[:search]).order('stambuk asc')
+    else
+        @students_biographies = StudentsBiography.all.paginate(:page => params[:page], per_page: 10).order('stambuk asc')
+    end
+  end
+
+  # GET /students_biographies/1
+  # GET /students_biographies/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_students_biography
+      @students_biography = StudentsBiography.find(params[:id])
+    end
+end
